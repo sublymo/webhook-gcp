a01e6261153bbb6466898bdfb48bad47187ccb2c
Abhideep Singh <abhideep@>
2023-11-17 10:22:02 +0530

    Setup a basic function for a notify webhook

---
+++ notify/index.js
@@ -0,0 +1,25 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+
+// The Firebase Admin SDK to access Firestore.
+const {initializeApp} = require("firebase-admin/app");
+const {getFirestore} = require("firebase-admin/firestore");
+
+initializeApp();
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+exports.notify = onRequest((request, response) => {
+   logger.info("Notification Received!", {structuredData: true});
+   response.send("Change Notification Received");
+});
